a0aa6837582cce00a7d40debb6d317157f2ea585
Hernan Gomez <95485080+nanreh1976@users.noreply.github.com>
2022-10-09 13:51:32 -0300

    usos de numeros en python

---
+++ numeros.py
@@ -0,0 +1,20 @@
+from math import trunc, pow, sqrt,pi
+from random import randint, uniform, random
+
+print(abs(-23.34)) # valor absoluto
+print(round(45.7834)) # valor redondeando casi siempre para arriba
+print(round(45.7865,2)) # valor redondeado con 2(n) decimales
+print(trunc(45.7865)) # valor real, sin su parta decimal sin redondear
+print(pow(2,3)) # calcular potencia
+print(sqrt(4)) # calcular raiz cuadrada
+print(pi) # el valor constante de pi
+print (randint(1,6)) #numero aleatorio entero de n a n1 (1,6)
+print (randint(1,6)) # ejemplo cara de los dados
+print (randint(1,6))
+print (randint(1,6))
+print (randint(1,6))
+print (randint(1,6))
+print (uniform(78.34,99.55)) # numero aletorio flotanto, intervalo cerrado [78.34,99.55]
+print (random()) #numero aletorio 
+
+
